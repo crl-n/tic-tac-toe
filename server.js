f3f1a9d0ad64f2bd45dca126270f65a9aa0bd9a3
Carl Nysten <nysten.carl@gmail.com>
2023-07-23 22:46:01 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const app = express();
+const port = 3030;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.send('Hi.');
+});
+
+app.listen(port, function () {
+    console.log(`Tic Tac Toe up and running at port ${port}`);
+});
