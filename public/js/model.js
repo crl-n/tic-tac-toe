89f925bbab2b316d9d4645194395ef2d9782a7ad
Carl Nysten <nysten.carl@gmail.com>
2023-07-24 21:40:59 +0300

    Model checks for terminal state

--- public/js/model.js
+++ public/js/model.js
@@ -1,5 +1,5 @@
-import Event from "./event.js";
 
+import Event from "./event.js";
 export default class TicTacToe {
 
     constructor() {
@@ -12,6 +12,12 @@ export default class TicTacToe {
         this.drawEvent = new Event();
     }
 
+    reset() {
+        this.board.fill();
+        this.currentPlayer = 'X';
+        this.finished = false;
+    }
+
     play(move) {
         if (this.board[move]) {
             return ;
@@ -19,7 +25,33 @@ export default class TicTacToe {
 
         this.board[move] = this.currentPlayer;
         this.updateCellEvent.trigger({ index: move, player: this.currentPlayer });
-        this.switchPlayer();
+
+        this.checkIfFinished(move);
+
+        if (!this.finished) {
+            this.switchPlayer();
+        }
+    }
+
+    checkIfFinished(move) {
+        const axes = {
+            horizontal: [0, 1, 2].map(i => i + Math.floor(move / 3) * 3),
+            vertical: [0, 3, 6].map(i => i + move % 3),
+            diagonal: [0, 4, 9],
+            counterDiagonal: [2, 4, 6]
+        };
+
+        const allCurrentPlayer = (mask) => {
+            return this.board.filter((value, i) => mask.includes(i))
+                .every((value, i, array) => value === this.currentPlayer);
+        }
+
+        for (const axis of Object.values(axes)) {
+            if (allCurrentPlayer(axis)) {
+                this.victoryEvent.trigger(this.currentPlayer);
+                this.finished = true;
+            }
+        }
     }
 
     switchPlayer() {
@@ -27,9 +59,5 @@ export default class TicTacToe {
             return this.currentPlayer = 'O';
         }
         this.currentPlayer = 'X';
-    }
-
-    victory() {
-
     }
 }
